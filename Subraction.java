3cdb31f3802c7c3c29e507ef996c2b42f29ed276
“bharath669” <vvbharath2006@gmail.com>
2025-06-30 11:40:48 +0100

    Subtraction file is Modified

--- Subraction.java
+++ Subraction.java
@@ -1,7 +1,5 @@
 public class Subraction {
     public static void main(String[] args) {
-        int a=1;
-        int b=2;
-        System.out.println(a+b);
+        System.out.println("Hello world");
     }
 }
