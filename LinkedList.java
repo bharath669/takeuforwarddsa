f8b3e5ace9f03d6723dc0da0db2dfd01b8badb1c
“bharath669” <vvbharath2006@gmail.com>
2025-12-16 14:07:54 +0530

    Array to LL

--- LinkedList.java
+++ LinkedList.java
@@ -1,16 +1,27 @@
 class Node{
     int data;
     Node next;
-    Node(int data,Node next){
+    Node(int data){
         this.data=data;
-        this.next=next;
+        this.next=null;
     }
-}
+};
 
 class LinkedList{
+    public static Node convertArr2LL(int[] arr){
+        Node head=new Node(arr[0]);
+        Node mover=head;
+        for(int i=1;i<arr.length;i++){
+            Node temp=new Node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+        return head;
+    }
     public static void main(String[] args){
         int[] arr={1,4,8,3};
-        Node n=new Node(arr[0], null);
-        System.out.println(n.data);
+        Node head=convertArr2LL(arr);
+        System.out.println(head.data);
+        
     }
 }
